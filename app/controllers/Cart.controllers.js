ea4c0f0688f3722f1648fb9a1c028e05e07210e5
ngocxxu <ngocquach43@gmail.com>
2022-11-19 13:16:32 +0700

    commit for cart

--- app/controllers/Cart.controllers.js
+++ app/controllers/Cart.controllers.js
@@ -1,5 +1,4 @@
 import { CartModel } from '../models/Cart.models.js';
-import { OrderModel } from '../models/Order.models.js';
 import { ProductModel } from '../models/Product.models.js';
 
 export const getCart = async (req, res) => {
@@ -95,28 +94,101 @@ export const updateToCart = async (req, res) => {
   try {
     let cart;
 
+    console.log(req.body.quantity);
+
     // Find idProduct exist in LineItems
     cart = await CartModel.findOneAndUpdate(
       {
         lineItems: { $elemMatch: { idProduct: req.body.idProduct } },
       },
+
       {
         $set: {
           'lineItems.$.subQuantity': req.body.quantity,
         },
-        $mul: {
-          'lineItems.$.subTotalProduct': req.body.quantity,
-        },
       }
     );
 
-    cart = await CartModel.findOne({ idCart: req.params.id }).lean();
-    const cartTotalPrice = cart.lineItems.reduce(
-      (a, b) => a + b.subTotalProduct,
-      0
-    );
+    cart = await CartModel.aggregate([
+      {
+        $match: {
+          idCart: req.params.id,
+        },
+      },
+      {
+        $unwind: '$lineItems',
+      },
+      {
+        $set: {
+          'lineItems.subTotalProduct': {
+            $multiply: [
+              '$lineItems.subQuantity',
+              {
+                $toInt: '$lineItems.product.price.raw',
+              },
+            ],
+          },
+        },
+      },
+    ]);
 
-    cart.subTotal = cartTotalPrice;
+    // cart = await CartModel.aggregate([
+    //   {
+    //     $match: {
+    //       lineItems: { $elemMatch: { idProduct: req.body.idProduct } },
+    //     },
+    //   },
+    //   { $unwind: '$lineItems' },
+    //   {
+    //     $project: {
+    //       idCart: 1,
+    //       totalItems: 1,
+    //       subTotal: 1,
+    //       discount: 1,
+    //       lineItems: 1,
+    //       // 'lineItems.subTotalProduct': {
+    //       //   $multiply: ['$lineItems.product.price.raw', req.body.quantity],
+    //       // },
+    //     },
+    //   },
+
+    //   // {
+    //   //   $project: {
+    //   //     idCart: 1,
+    //   //     totalItems: 1,
+    //   //     subTotal: 1,
+    //   //     discount: 1,
+    //   //     lineItems: {
+    //   //       $filter: {
+    //   //         product,
+    //   //         idProduct: req.body.idProduct,
+    //   //         subQuantity: req.body.quantity,
+    //   //         subTotalProduct: req.body.price,
+    //   //       },
+    //   //     },
+    //   //   },
+    //   // },
+    //   //   $project: {
+    //   //     'lineItems.$.subTotalProduct': {
+    //   //       $multiply: ['lineItems.$.price', req.body.quantity],
+    //   //     },
+    //   //   },
+    // ]);
+
+    console.log(cart);
+
+    // cart = await CartModel.findOne({ idCart: req.params.id }).lean();
+    // const cartTotalPrice = cart.lineItems.reduce(
+    //   (a, b) => a + b.subTotalProduct,
+    //   0
+    // );
+
+    // cart = await CartModel.findOneAndUpdate(
+    //   { idCart: req.params.id },
+    //   {
+    //     $set: { subTotal: cartTotalPrice },
+    //   }
+    // );
 
     res.status(200).json(cart);
   } catch (err) {
@@ -126,8 +198,6 @@ export const updateToCart = async (req, res) => {
 
 export const removeToCart = async (req, res) => {
   try {
-    console.log(req.params.id);
-    console.log(req.params.idProduct);
     let cart;
     cart = await CartModel.updateOne(
       { idCart: req.params.id },
@@ -146,7 +216,12 @@ export const removeToCart = async (req, res) => {
       0
     );
 
-    cart.subTotal = cartTotalPrice;
+    cart = await CartModel.findOneAndUpdate(
+      { idCart: req.params.id },
+      {
+        $set: { subTotal: cartTotalPrice },
+      }
+    );
 
     res.status(200).json(cart);
   } catch (err) {
